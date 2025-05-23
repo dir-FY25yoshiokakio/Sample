12fe4e3d492ef51841570b78b44ed7916dd148cb
kio.yoshioka <kio.yoshioka.j627@dirbato.co.jp>
2025-05-23 14:07:27 +0900

    if

---
+++ Sample/src/sample/IfSample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class IfSample {
+    public static void main(String[] args){
+        var score = 80;
+        
+        if(score >= 60) {
+            System.out.println("合格です！");
+        }
+    }
+}
