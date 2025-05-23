503059d2fce97624b6f2804dd1c63b6f55f55d8d
kio.yoshioka <kio.yoshioka.j627@dirbato.co.jp>
2025-05-23 14:15:13 +0900

    else if6.3

--- Sample/src/sample/IfSample.java
+++ Sample/src/sample/IfSample.java
@@ -2,9 +2,13 @@ package sample;
 
 public class IfSample {
     public static void main(String[] args){
-        var score = 50;//成瀬
+        var score = 80;//成瀬
         
         if(score >= 60) {
+            System.out.println("合格です！");
+        }else if(score >= 80){
+            System.out.println("よくできました！");
+        }else if(score == 100){
             System.out.println("合格です！");
         }else {
             System.out.println("赤点です。。。");
