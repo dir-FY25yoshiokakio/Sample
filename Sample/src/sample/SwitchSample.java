81cf74446d8fb329243c044179b02e0697e9b5fb
kio.yoshioka <kio.yoshioka.j627@dirbato.co.jp>
2025-05-23 14:30:30 +0900

    switch ver2

--- Sample/src/sample/SwitchSample.java
+++ Sample/src/sample/SwitchSample.java
@@ -6,10 +6,19 @@ public class SwitchSample {
         var score = 72;
         
         switch (score/10) {
-            case 10 -> System.out.println("満点です！");
-            case 9,8 -> System.out.println("よくできました！");
-            case 7,6 -> System.out.println("合格です！");
-            default -> System.out.println("赤点です。。。");
+        case 10: 
+            System.out.println("満点です！"); 
+            break; 
+            case 9: 
+            case 8: 
+            System.out.println("よくできました！"); 
+            break; 
+            case 7: 
+            case 6: 
+            System.out.println("合格です！"); 
+            break; 
+            default: 
+            System.out.println("赤点です。。。");
         }
 
     }
