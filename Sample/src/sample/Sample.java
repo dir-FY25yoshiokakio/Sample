295dffbf7c638697fa97feccb52cec50e338fc68
kio.yoshioka <kio.yoshioka.j627@dirbato.co.jp>
2025-05-23 12:17:36 +0900

    newclass sample

---
+++ Sample/src/sample/Sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sample {
+    
+    public static void main(String[] args){
+        var secondsPerDay = 24 * 60 * 60;
+        
+        System.out.println(secondsPerDay);
+    }
+
+}
